918fb0836f8b9a126d7e0f1d580da9ff890a7ca2
Ashton <ashton.sickler@icloud.com>
2023-09-11 09:16:26 -0500

    Added draw() to player, added show_hand() to player, init deck with cards

--- rumm.py
+++ rumm.py
@@ -1,6 +1,16 @@
 class Deck:
     def __init__(self) -> None:
-        self.num_of_cards = 52;
+
+        suits = ['S', 'H', 'C', 'D']
+        values = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
+
+        self.deck_contents = []
+
+        for i in suits:
+            for j in values:
+                self.deck_contents += [(j, i)]
+        
+        self.num_of_cards = len(self.deck_contents)
 
     def __repr__(self) -> str:
         return "Deck has " + str(self.num_of_cards) + " cards left"
@@ -10,7 +20,17 @@ class Player:
     def __init__(self):
         self.id = id(self)
         self.hand = []
-        d1.num_of_cards -= 7
+
+        for c in range(7):
+            self.hand += d1.deck_contents.pop()
+        
+    
+    def draw(self):
+        self.hand += d1.deck_contents.pop()
+
+    def show_hand(self):
+        for card in self.hand:
+            print(card)
 
 # --- MUST BE INITIALIZED TO RUN --- #
 # ---       DO NOT REMOVE        --- #
@@ -18,4 +38,6 @@ d1 = Deck()
 
 p1 = Player()
 
+print(d1)
+p1.show_hand()
 print(d1)
